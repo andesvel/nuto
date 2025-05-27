2e09072091efc0082d38df4904d3cfa7f7996b01
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-05-27 08:51:44 -0500

    feat: implement Shorten component for URL shortening functionality

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
+import React from "react";
+import Shorten from "@components/shorten";
 
 export default function Hero() {
-  const [url, setUrl] = useState("");
-
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
-      <div className="px-4 md:px-6 z-10">
-        <div className="mx-auto max-w-3xl text-center">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
-            Short links with <span className="text-primary">ease</span>
-          </h1>
-          <p className="mb-10 text-lg text-gray-600 md:text-xl">
-            Transform your long URLs into concise, memorable links.
-          </p>
-        </div>
+    <section className="w-full grow -mt-52">
+      <div className="mx-auto max-w-3xl text-center">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
+          Short links with <span className="text-primary">ease</span>
+        </h1>
+        <p className="mb-10 text-lg text-muted-foreground md:text-xl">
+          Nuto transforms your long URLs into concise, memorable links.
+        </p>
       </div>
+      <Shorten />
     </section>
   );
 }
