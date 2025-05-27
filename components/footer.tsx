9c4cf6588ef2525494a499e69c27830bd2eeae5d
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-05-27 08:51:55 -0500

    refactor: clean up Footer and Header components by removing unused imports and adjusting class names for consistency

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,32 +1,13 @@
+import * as React from "react";
 import { Link } from "react-router";
-import {
-  SiFacebook,
-  SiX,
-  SiInstagram,
-  SiLinkerd,
-  SiGithub,
-} from "@icons-pack/react-simple-icons";
+import { SiGithub } from "@icons-pack/react-simple-icons";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-black/5 backdrop-blur-lg py-4 px-6">
-      <div className="flex items-center justify-between center-root">
-        <p>Made with ❤️ using Cloudflare Workers by Andres Estrada</p>
+    <footer className="w-full border-t border-black/5 backdrop-blur-xl backdrop-saturate-150 py-4 px-6">
+      <div className="flex items-center justify-between c-root">
+        <p>Made with ❤️ by Andres using Workers</p>
         <div className="flex space-x-4">
-          <Link
-            to="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiFacebook />
-          </Link>
-          <Link
-            to="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SiInstagram />
-          </Link>
           <Link
             to="https://github.com"
             target="_blank"
