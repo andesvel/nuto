4d37d78030e563338680ec4f81f39f732a3b4b20
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-05-27 08:51:24 -0500

    fix: update meta title for consistency and adjust class names for styling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import type { Route } from "./+types/home";
 import Header from "@components/header";
 import Hero from "@components/hero";
 import Footer from "@/components/footer";
+import { action } from "@/components/shorten";
 
+// eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Nuto: Open source URL shortener" },
+    { title: "Nuto - An open source URL shortener" },
     {
       name: "description",
       content: "A simple and efficient URL shortening service.",
@@ -13,19 +16,17 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
+export { action };
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home() {
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-between antialiased">
+    <div className="flex min-h-dvh flex-col items-center justify-between antialiased">
       <Header />
-      <main className="center-root">
+      <main className="c-root grow flex items-center justify-center">
         <Hero />
       </main>
       <Footer />
-      <div className="absolute inset-0 -z-10 bg-[linear-gradient(to_right,rgba(from_var(--muted-foreground)_r_g_b_/_0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(from_var(--muted-foreground)_r_g_b_/_0.05)_1px,transparent_1px)] bg-[size:2rem_2rem]"></div>
+      <div className="absolute inset-0 -z-10 bg-[linear-gradient(to_right,rgba(from_var(--muted-foreground)_r_g_b_/_0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(from_var(--muted-foreground)_r_g_b_/_0.05)_1px,transparent_1px)] bg-[size:1lh_1lh]"></div>
     </div>
   );
 }
