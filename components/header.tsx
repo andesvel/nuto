e25f35c910a71ac3d3e5279988b1e0531939f342
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-12 16:08:00 -0500

    feat: enhance Header component with search functionality and improved layout

--- components/header.tsx
+++ components/header.tsx
@@ -9,37 +9,62 @@ import {
 } from "@clerk/react-router";
 import { LogIn } from "lucide-react";
 import { Button } from "./ui/button";
+import { Input } from "@ui/input";
+import { Plus, Search } from "lucide-react";
 
-export default function Header() {
+export default function Header({ onDashboard }: { onDashboard?: boolean }) {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-black/5 backdrop-blur-xl backdrop-saturate-150 supports-[backdrop-filter]:bg-card/30">
-      <div className="flex h-16 items-center justify-between c-root">
-        <div className="flex items-center gap-2">
-          <Link to="/" className="flex items-center gap-2">
-            <div className="relative flex h-8 w-8 items-center justify-center rounded-md bg-black text-white">
-              <span className="text-lg font-bold">N</span>
-            </div>
-            <span className="text-xl font-bold tracking-tight">Nuto</span>
-          </Link>
-        </div>
+    <header className="mb-4 sticky top-0 z-50 w-full backdrop-blur-md backdrop-saturate-150 supports-[backdrop-filter]:bg-card/30">
+      <div className="c-root flex flex-col">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Link to="/" className="flex items-center gap-2">
+              <div className="relative flex h-8 w-8 items-center justify-center rounded-md bg-black text-white">
+                <span className="text-lg font-bold">N</span>
+              </div>
+              <span className="text-xl font-bold tracking-tight">Nuto</span>
+            </Link>
+          </div>
 
-        <div className="flex items-center gap-4">
-          <ThemeSwitcher />
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton mode="modal">
-              <Button variant="ghost" className="group">
-                <LogIn
-                  size={18}
-                  className="duration-300 group-hover:translate-x-0.5"
-                />
-                Sign In
-              </Button>
-            </SignInButton>
-          </SignedOut>
+          <div className="flex items-center gap-4">
+            <ThemeSwitcher />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton mode="modal">
+                <Button variant="ghost" className="group">
+                  <LogIn
+                    size={18}
+                    className="duration-300 group-hover:translate-x-0.5"
+                  />
+                  Sign In
+                </Button>
+              </SignInButton>
+            </SignedOut>
+          </div>
         </div>
+        {onDashboard && (
+          <header className="h-full flex justify-between py-4 items-center gap-2 border-b md:border-b-0">
+            <div className="flex-1 relative md:max-w-1/2 lg:max-w-1/4">
+              <Search
+                className="absolute z-10 left-2 top-1/2 -translate-y-1/2 transform text-muted-foreground"
+                size="16"
+                strokeWidth={2}
+              />
+              <Input
+                type="search"
+                autoComplete="off"
+                placeholder="Search your links"
+                className="pl-8"
+              />
+            </div>
+            <Button>
+              <Plus strokeWidth={2} />
+              <span className="hidden md:inline-block">New Link</span>
+            </Button>
+          </header>
+        )}
       </div>
     </header>
   );
