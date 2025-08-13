3f310e86f280e761131bd0e9af49475912e317d8
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-13 12:33:26 -0500

    feat: add route for webhooks clerk

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
   route("dashboard", "routes/dashboard.tsx"),
   route("sign-in", "routes/sign-in.tsx"),
   route("sign-up", "routes/sign-up.tsx"),
+  route("webhooks/clerk", "routes/webhooks.clerk.tsx"),
 ] satisfies RouteConfig;
