eaa59aae4d0fb0e56e0f68d0b62a1e923da8b078
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-13 12:33:10 -0500

    fix: update import statement for CreateLink component

--- components/header.tsx
+++ components/header.tsx
@@ -12,7 +12,7 @@ import { Button } from "./ui/button";
 import { Input } from "@ui/input";
 import { Search } from "lucide-react";
 
-import { CreateLink } from "@components/links/create-link";
+import CreateLink from "@components/links/create-link";
 
 export default function Header({ onDashboard }: { onDashboard?: boolean }) {
   return (
