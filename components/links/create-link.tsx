d08c15bb91cc861ccb4fee114cb27c667ccad7c8
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-13 12:32:57 -0500

    feat: integrate generateShortCode function and enhance CreateLink component

--- components/links/create-link.tsx
+++ components/links/create-link.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import { useFetcher, type AppLoadContext, useLoaderData } from "react-router";
+
+import { generateShortCode } from "@utils/generate-short-code";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -14,23 +19,34 @@ import { Label } from "@/components/ui/label";
 
 import { Plus, Shuffle } from "lucide-react";
 
-export function CreateLink() {
+// export async function action() {}
+
+export default function CreateLink() {
+  const fetcher = useFetcher();
+  const busy = fetcher.state !== "idle";
+
+  const initialData = useLoaderData() as {
+    longUrl: string;
+    shortCode: string;
+    password?: string;
+  };
+
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button>
-            <Plus strokeWidth={2} />
-            <span className="hidden md:inline-block">New Link</span>
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Create a new short link</DialogTitle>
-            {/* <DialogDescription>
-              Create a new short link by filling out the form below.
-            </DialogDescription> */}
-          </DialogHeader>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus strokeWidth={2} />
+          <span className="hidden md:inline-block">New Link</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create a new short link</DialogTitle>
+          <DialogDescription className="sr-only">
+            Create a new short link by filling out the form below.
+          </DialogDescription>
+        </DialogHeader>
+        <fetcher.Form id="create-link-form">
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label htmlFor="longUrl">Long URL</Label>
@@ -41,7 +57,6 @@ export function CreateLink() {
                 name="longUrl"
                 placeholder="https://"
                 autoComplete="off"
-                // defaultValue="https://www.example.com"
               />
             </div>
             <div className="grid gap-3">
@@ -56,10 +71,14 @@ export function CreateLink() {
                   autoComplete="off"
                 />
                 <Button
+                  type="button"
                   variant="outline"
                   className="rounded-l-none"
                   onClick={() => {
-                    // Randomize short code
+                    const shortCode = generateShortCode();
+                    if (fetcher.formData) {
+                      fetcher.formData.set("shortCode", shortCode);
+                    }
                   }}
                 >
                   <Shuffle strokeWidth={2} />
@@ -74,17 +93,20 @@ export function CreateLink() {
                 name="password"
                 type="password"
                 placeholder="Protect this link"
+                autoComplete="new-password"
               />
             </div>
           </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Create</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+        </fetcher.Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit" form="create-link-form" disabled={busy}>
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
