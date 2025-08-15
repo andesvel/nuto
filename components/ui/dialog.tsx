51875089df60124917440da403380d14d0a0d7af
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-14 22:10:59 -0500

    fix: update background color for AlertDialog and Dialog overlays

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -36,7 +36,7 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-muted/25 backdrop-blur-md backdrop-filter",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-muted/40 backdrop-blur-md backdrop-filter",
         className
       )}
       {...props}
