07b16c7ea2bf9f3d11a885a401730440a9596b84
Andres Estrada <105224908+andesvel@users.noreply.github.com>
2025-08-22 22:35:44 -0500

    fix: update Footer layout for improved responsiveness and structure

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { Link } from "react-router";
 import { SiGithub } from "@icons-pack/react-simple-icons";
 import { ExternalLink } from "lucide-react";
+import { Separator } from "@ui/separator";
 
 export default function Footer() {
   return (
     <footer className="c-root w-full backdrop-blur-xl backdrop-saturate-150 py-4">
-      <div className="flex items-center justify-between text-sm text-muted-foreground">
+      <div className="text-sm text-muted-foreground flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <p>
           Made by Andres using{" "}
           <Link
@@ -19,15 +20,43 @@ export default function Footer() {
             <ExternalLink className="ml-1 inline" size={14} />
           </Link>
         </p>
-        <div className="flex space-x-4">
-          <Link
-            to="https://github.com/andesvel/nuto"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="sr-only">Nuto on GitHub</span>
-            <SiGithub className="hover:text-foreground transition-colors duration-200 ease-in-out" />
-          </Link>
+        <div className="flex items-center space-x-4 w-full sm:w-auto justify-between">
+          {/* <div className="flex space-x-4">
+            <Link
+              to="https://TODO"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Disclaimer
+            </Link>
+          </div> */}
+          <div className="flex space-x-4">
+            <Link
+              to="https://github.com/andesvel/nuto/issues/new?assignees=&labels=bug&template=bug_report.md&title="
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Report a bug
+              <span className="sr-only">Report a bug</span>
+              <ExternalLink className="ml-1 inline" size={14} />
+            </Link>
+          </div>
+          <div className="h-5 flex space-x-4">
+            <Separator
+              orientation="vertical"
+              className="hidden sm:inline-block"
+            />
+            <Link
+              to="https://github.com/andesvel/nuto"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="sr-only">Nuto on GitHub</span>
+              <SiGithub className="hover:text-foreground transition-colors duration-200 ease-in-out" />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
